ffdc40b1fc486d909fdbaa818702d8bef2e91c4d
Luki R Daniel <thekucays@gmail.com>
2016-04-21 17:48:47 +0700

    Tutorial 11 - (listOfAddress type changed to ArrayList in order to support primary key, there are also @CollectionId and @GenericGenerator added for primary key generator)

--- src/test/java/com/luki/entities/UserDetails.java
+++ src/test/java/com/luki/entities/UserDetails.java
@@ -1,6 +1,8 @@
 package com.luki.entities;
 
 // use this instead of hibernate's annotation class
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -19,6 +21,10 @@ import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 /*
  * to generate getters and setters,
@@ -39,10 +45,16 @@ public class UserDetails {
 	private String description;
 	@ElementCollection
 	@JoinTable(
-			name="user_address",					// ini untuk kasih nama tabel untuk address nya (default nya namaclass_namahashset -> UserDetails_listOfAddress)
-			joinColumns=@JoinColumn(name="user_id")	// ini untuk kasih nama kolom foreign key nya (user id nya)
+		name="user_address",						// ini untuk kasih nama tabel untuk address nya (default nya namaclass_namahashset -> UserDetails_listOfAddress)
+		joinColumns=@JoinColumn(name="user_id")		// ini untuk kasih nama kolom foreign key nya (user id nya)
 	)
-	private Set<Address> listOfAddresses = new HashSet();
+	@GenericGenerator(								// ini untuk generator primary key address nya yang dipake sama @CollectionId..please note kalau ini import nya dari org.hibernate dan bukan javax.persistence
+		name = "hilo-gen", strategy = "hilo"		// hilo ini standard hibernate untuk generator id nya.. (pelajari lagi jenis strategi nya apa lagi)
+	)
+	@CollectionId(									// untuk bikin id nya.. please note kalau ini import nya dari org.hibernate dan bukan javax.persistence
+		columns = { @Column(name="address_id") }, generator = "hilo-gen", type = @Type(type = "long")
+	)
+	private Collection<Address> listOfAddresses = new ArrayList<Address>( ); // supaya support id nya, dirubah jadi pake ArrayList 
 	
 	//@Embedded
 		//private Address address;
@@ -57,14 +69,20 @@ public class UserDetails {
 	public String getDescription() {
 		return description;
 	}
-	public Set<Address> getListOfAddresses() {
+	/*public Set<Address> getListOfAddresses() {
 		return listOfAddresses;
 	}
 	public void setListOfAddresses(Set<Address> listOfAddresses) {
 		this.listOfAddresses = listOfAddresses;
-	}
+	}*/
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	public Collection<Address> getListOfAddresses() {
+		return listOfAddresses;
+	}
+	public void setListOfAddresses(Collection<Address> listOfAddresses) {
+		this.listOfAddresses = listOfAddresses;
 	}
 	public Date getJoinedDate() {
 		return joinedDate;
